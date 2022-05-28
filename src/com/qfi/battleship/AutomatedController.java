9999e5be152258cdc27ad2dfdcbecece3a2e66e5
xTriixrx <vnigro5743@gmail.com>
2022-05-28 13:53:51 -0400

    Better AI mechanics, still a bit buggy working through issues

--- src/com/qfi/battleship/AutomatedController.java
+++ src/com/qfi/battleship/AutomatedController.java
@@ -23,6 +23,7 @@ public class AutomatedController implements Runnable, Observer, Observable, Cont
 	private Object turnMutex = null;
 	private Observer observer = null;
 	private boolean shutdown = false;
+	private boolean shipFocus = false;
 	private boolean myTurnFlag = false;
 	private SecureRandom random = null;
 	private boolean isShipsSet = false;
@@ -32,16 +33,20 @@ public class AutomatedController implements Runnable, Observer, Observable, Cont
 	private boolean isDestroyerSunk = false;
 	private boolean isBattleshipSunk = false;
 	private ArmadaAutomator automator = null;
+	private List<String> hitShipPositions = null;
 	private List<String> guessedPositions = null;
-	private List<String> guessedPositionsOutcome = null;
 	private Logger logger = LogManager.getLogger(AutomatedController.class);
 	
 	private static final int clientTurn = 1;
 	private static final int serverTurn = 2;
 	private static final String HIT = "Hit";
 	private static final String MISS = "Miss";
+	private static final int UP_DIRECTION = 2;
 	private static final char clientSymbol = 'Z';
 	private static final char serverSymbol = 'X';
+	private static final int LEFT_DIRECTION = -1;
+	private static final int RIGHT_DIRECTION = 1;
+	private static final int DOWN_DIRECTION = -2;
 	
 	/**
 	 * 
@@ -52,7 +57,7 @@ public class AutomatedController implements Runnable, Observer, Observable, Cont
 		armada = new Armada();
 		turnMutex = new Object();
 		guessedPositions = new ArrayList<>();
-		guessedPositionsOutcome = new ArrayList<>();
+		hitShipPositions = new ArrayList<>();
 		
 		automator = new ArmadaAutomator(armada);
 		automator.automateArmadaPlacement();
@@ -107,19 +112,181 @@ public class AutomatedController implements Runnable, Observer, Observable, Cont
 	
 	public void makeGuess(int ammo)
 	{
-		if (ammo > 0)
+		logger.info("AUTOMATEDCONTROLLER: GUESSED POSITIONS: {}", guessedPositions);
+		
+		if (isShipInFocus())
 		{
-			String lastOutcome = guessedPositionsOutcome.get(--ammo);
+			String position = "";
+			String lastPosition = latestGuess;
+			logger.info("HIT SHIP POSITIONS: " + hitShipPositions);
 			
-			if (lastOutcome == HIT)
+			if (hitShipPositions.contains(lastPosition) && hitShipPositions.size() > 1) // hit multiple sections
 			{
-				// Need to get create logic to focus on hit position until ship has sunk.
-				performRandomPositionProtocol();
-				return;
+				logger.info("AUTOMATEDCONTROLLER: LAST POSITION HIT & MULTIPLE HITS");
+				String prevHitPosition = hitShipPositions.get(hitShipPositions.indexOf(lastPosition) - 1);
+				
+				position = getNextPosition(lastPosition,
+					calculateDirection(lastPosition, prevHitPosition));
+				
+				if (guessedPositions.contains(position))
+				{
+					String firstHitPosition = hitShipPositions.get(0);
+					String nextHitPosition = hitShipPositions.get(hitShipPositions.indexOf(firstHitPosition) + 1);
+					
+					position = getNextPosition(firstHitPosition,
+						calculateDirection(firstHitPosition, nextHitPosition));
+				}
 			}
+			else if (!hitShipPositions.contains(lastPosition) && hitShipPositions.size() > 1) // ran off
+			{
+				logger.info("AUTOMATEDCONTROLLER: LAST POSITION MISSED & MULTIPLE HITS");
+				
+				String firstHitPosition = hitShipPositions.get(0);
+				String nextHitPosition = hitShipPositions.get(hitShipPositions.indexOf(firstHitPosition) + 1);
+				
+				position = getNextPosition(firstHitPosition,
+					calculateDirection(firstHitPosition, nextHitPosition));
+				
+				if (guessedPositions.contains(position))
+				{
+					position = getCrossPosition(firstHitPosition);
+				}
+			}
+			else
+			{
+				logger.info("AUTOMATEDCONTROLLER: CROSS POSITION");
+				position = getCrossPosition(hitShipPositions.get(0));
+			}
+
+			latestGuess = position;
+			guessedPositions.add(latestGuess);
+			logger.info("AUTOMATEDCONTROLLER: GUESS: {}", latestGuess);
+			observer.update(latestGuess);
+			
+			return;
 		}
 		
 		performRandomPositionProtocol();
+	}
+	
+	private String getNextPosition(String contextPosition, int direction)
+	{
+		String position = "";
+		char columnChar = 'a';
+		contextPosition = contextPosition.substring(1, contextPosition.length() - 1);
+		
+		int column = (int) (contextPosition.charAt(0) - 64);
+		int row = Integer.parseInt(contextPosition.substring(1));
+		
+		if (direction == LEFT_DIRECTION)
+		{
+			column--;
+		}
+		else if (direction == RIGHT_DIRECTION)
+		{
+			column++;
+		}
+		else if (direction == UP_DIRECTION)
+		{
+			row++;
+		}
+		else if (direction == DOWN_DIRECTION)
+		{
+			row--;
+		}
+		
+		columnChar = (char) (column + 64);
+		
+		position = "0";
+		position += columnChar;
+		position += row;
+		position += mySymbol;
+		
+		return position;
+	}
+	
+	private int calculateDirection(String p1, String p2)
+	{
+		int direction = 0;
+		p1 = p1.substring(1, p1.length() - 1);
+		p2 = p2.substring(1, p2.length() - 1);
+		
+		int c1 = (int) (p1.charAt(0) - 64);
+		int c2 = (int) (p2.charAt(0) - 64);
+		int r1 = Integer.parseInt(p1.substring(1));
+		int r2 = Integer.parseInt(p2.substring(1));
+		
+		if (c1 > c2)
+		{
+			direction = RIGHT_DIRECTION;
+		}
+		else if (c2 > c1)
+		{
+			direction = LEFT_DIRECTION;
+		}
+		else if (r1 > r2)
+		{
+			direction = UP_DIRECTION;
+		}
+		else if (r2 > r1)
+		{
+			direction = DOWN_DIRECTION;
+		}
+		
+		logger.info("AUTOMATEDCONTROLLER: DIRECTION: {}", direction);
+		
+		return direction;
+	}
+	
+	private String getCrossPosition(String focusPosition)
+	{
+		String position = "";
+		focusPosition = focusPosition.substring(1, focusPosition.length() - 1);
+		
+		do
+		{
+			int column = (int) (focusPosition.charAt(0) - 64);
+			int row = Integer.parseInt(focusPosition.substring(1));
+			
+			logger.info("AUTOMATEDCONTROLLER: Captured Row: {}.", row);
+			logger.info("AUTOMATEDCONTROLLER: Captured column: {}.", column);
+			
+			int upOrDown = random.nextInt(2) + 1;
+			int verticalOrHorizontal = random.nextInt(2) + 1;
+			
+			if (verticalOrHorizontal == 1) // vertical
+			{
+				if (upOrDown == 1) // up
+				{
+					row++;
+				}
+				else // down
+				{
+					row--;
+				}
+			}
+			else // horizontal
+			{
+				if (upOrDown == 1) // left column
+				{
+					column--;
+				}
+				else // right column
+				{
+					column++;
+				}
+			}
+			
+			char columnChar = (char) (column + 64);
+			
+			position = "O";
+			position += columnChar;
+			position += row;
+			position += mySymbol;
+			logger.info("Attempting random position: {}", position);
+		} while (guessedPositions.contains(position));
+		
+		return position;
 	}
 	
 	private void performRandomPositionProtocol()
@@ -166,26 +333,44 @@ public class AutomatedController implements Runnable, Observer, Observable, Cont
 		else if (update.equals(Armada.CARRIER_NAME))
 		{
 			logger.info("AUTOMATEDCONTROLLER: {}", Armada.CARRIER_NAME);
+			hitShipPositions.clear();
 		}
 		else if (update.equals(Armada.BATTLESHIP_NAME))
 		{
 			logger.info("AUTOMATEDCONTROLLER: {}", Armada.BATTLESHIP_NAME);
+			hitShipPositions.clear();
 		}
 		else if(update.equals(Armada.CRUISER_NAME))
 		{
 			logger.info("AUTOMATEDCONTROLLER: {}", Armada.CRUISER_NAME);
+			hitShipPositions.clear();
 		}
 		else if(update.equals(Armada.SUBMARINE_NAME))
 		{
 			logger.info("AUTOMATEDCONTROLLER: {}", Armada.SUBMARINE_NAME);
+			hitShipPositions.clear();
 		}
 		else if(update.equals(Armada.DESTROYER_NAME))
 		{
 			logger.info("AUTOMATEDCONTROLLER: {}", Armada.DESTROYER_NAME);
+			hitShipPositions.clear();
 		}
 		else if (getCurrentTurn() == myTurn && myTurnStatus())
 		{
-			guessedPositionsOutcome.add(update);
+			if (update.equalsIgnoreCase(HIT) && !isShipInFocus())
+			{
+				setShipFocus(true);
+			}
+			else if (update.equalsIgnoreCase(HIT) && isShipInFocus() && hitShipPositions.isEmpty())
+			{
+				setShipFocus(false);
+			}
+			
+			if (update.equalsIgnoreCase(HIT) && isShipInFocus())
+			{
+				hitShipPositions.add(latestGuess);
+			}
+			
 			StringBuilder temp = new StringBuilder(latestGuess);
 
 			String t = "";
@@ -266,6 +451,26 @@ public class AutomatedController implements Runnable, Observer, Observable, Cont
 		}
 		
 		return false;
+	}
+	
+	private void setShipFocus(boolean focus)
+	{
+		synchronized (turnMutex)
+		{
+			shipFocus = focus;
+		}
+	}
+	
+	private boolean isShipInFocus()
+	{
+		boolean focus = false;
+		
+		synchronized (turnMutex)
+		{
+			focus = shipFocus;
+		}
+		
+		return focus;
 	}
 	
 	private void setTurnStatus(boolean t)
