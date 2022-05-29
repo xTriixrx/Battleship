197a07875cf4c412160bbd13f409f732b27b74ca
xTriixrx <vnigro5743@gmail.com>
2022-05-29 17:26:39 -0400

    Updated batch scripts to support automated flag

--- scripts/startClient.bat
+++ scripts/startClient.bat
@@ -4,18 +4,19 @@ set JAVAFX_LIB=%1
 set GAME_LIB=%2
 set HOST=%3
 set PORT=%4
+set OPPONENT=%5
 set ARGC=0
 
 for %%x in (%*) do Set /A ARGC+=1
 
-if not %ARGC% == 4 (
-	echo Expected 4 arguments but only received %ARGC% arguments.
-	echo ./startClient.bat [JAVAFX_LIB] [GAME_LIB] [HOST] [PORT]
-	echo ./startClient.bat C:\javafx-sdk-18.0.1\lib lib 192.168.5.1 5000
+if not %ARGC% == 5 (
+	echo Expected 5 arguments but only received %ARGC% arguments.
+	echo ./startClient.bat [JAVAFX_LIB] [GAME_LIB] [HOST] [PORT] [AUTOMATED]
+	echo ./startClient.bat C:\javafx-sdk-18.0.1\lib lib 192.168.5.1 5000 false
 	EXIT /B
 )
 
 START /B java --module-path %JAVAFX_LIB% --add-modules javafx.controls,javafx.fxml -cp ^
-	"%JAVAFX_LIB%\*;%GAME_LIB%/*" com.qfi.battleship.GUIDriverRunner --type=client --host=%HOST% --port=%PORT%
+	"%JAVAFX_LIB%\*;%GAME_LIB%/*" com.qfi.battleship.GUIDriverRunner --type=client --host=%HOST% --port=%PORT% --automatedOpponent=%OPPONENT%
 
 EXIT /B
--- scripts/startServer.bat
+++ scripts/startServer.bat
@@ -3,18 +3,19 @@
 set JAVAFX_LIB=%1
 set GAME_LIB=%2
 set PORT=%3
+set OPPONENT=%4
 set ARGC=0
 
 for %%x in (%*) do Set /A ARGC+=1
 
-if not %ARGC% == 3 (
-	echo Expected 3 arguments but only received %ARGC% arguments.
-	echo ./startServer.bat [JAVAFX_LIB] [GAME_LIB] [PORT]
-	echo ./startServer.bat C:\javafx-sdk-18.0.1\lib lib 5000
+if not %ARGC% == 4 (
+	echo Expected 4 arguments but only received %ARGC% arguments.
+	echo ./startServer.bat [JAVAFX_LIB] [GAME_LIB] [PORT] [AUTOMATED]
+	echo ./startServer.bat C:\javafx-sdk-18.0.1\lib lib 5000 false
 	EXIT /B
 )
 
 START /B java --module-path %JAVAFX_LIB% --add-modules javafx.controls,javafx.fxml -cp ^
-	"%JAVAFX_LIB%\*;%GAME_LIB%/*" com.qfi.battleship.GUIDriverRunner --type=server --port=%PORT% --host=127.0.0.1
+	"%JAVAFX_LIB%\*;%GAME_LIB%/*" com.qfi.battleship.GUIDriverRunner --type=server --port=%PORT% --host=127.0.0.1 --automatedOpponent=%OPPONENT%
 
 EXIT /B
