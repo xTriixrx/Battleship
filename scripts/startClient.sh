1a381fdb6e33bf5759213b5c7d3f13bbfc72e692
xTriixrx <vnigro5743@gmail.com>
2022-07-27 22:33:38 -0400

    Linux start scripts tested & pom updates

---
+++ scripts/startClient.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+HOST="$3"
+PORT="$4"
+OPPONENT="$5"
+GAME_LIB="$2"
+JAVAFX_LIB="$1"
+
+if [ $# -ne 5 ]; then
+	echo "Expected 5 parameters but only received $# parameters."
+	echo "./startClient.sh [JAVAFX_LIB] [GAME_LIB] [HOST] [PORT] [AUTOMATED]"
+	echo "./startClient.sh javafx-17.0.2/lib lib 192.168.0.1 5000 false"
+	exit 1
+fi
+
+java --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
+-cp "$JAVAFX_LIB/*:$GAME_LIB/*" com.qfi.battleship.GUIDriverRunner --type=client --port=$PORT --host=$HOST --automatedOpponent=$OPPONENT &
+
+exit 0
---
+++ scripts/startServer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+PORT="$3"
+OPPONENT="$4"
+GAME_LIB="$2"
+JAVAFX_LIB="$1"
+
+if [ $# -ne 4 ]; then
+	echo "Expected 4 parameters but only received $# parameters."
+	echo "./startServer.sh [JAVAFX_LIB] [GAME_LIB] [PORT] [AUTOMATED]"
+	echo "./startServer.sh javafx-17.0.2/lib lib 5000 false"
+	exit 1
+fi
+
+java --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
+-cp "$JAVAFX_LIB/*:$GAME_LIB/*" com.qfi.battleship.GUIDriverRunner --type=server --port=$PORT --host=127.0.0.1 --automatedOpponent=$OPPONENT &
+
+exit 0
