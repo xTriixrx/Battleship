43782ad7fbe7a7902aeaeb08d2befc818e8e993d
xTriixrx <vnigro5743@gmail.com>
2022-10-13 21:31:26 -0400

    Refinement & comments

--- src/com/qfi/battleship/GUIDriver.java
+++ src/com/qfi/battleship/GUIDriver.java
@@ -1,6 +1,7 @@
 package com.qfi.battleship;
 
 import java.util.Map;
+import java.util.Objects;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
@@ -10,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 
 /**
- * GUIDriver is an generic Application class which will instantiate a Battleship FXML layout in coordination with
+ * GUIDriver is a generic Application class which will instantiate a Battleship FXML layout in coordination with
  * an associated communication logic thread and board controller class. One instance of the GUIDriver is for a single
  * player; the first player to start their application should start a server instance while the second instance should
  * be a client instance.
@@ -22,21 +23,17 @@ public class GUIDriver extends Application
 {
 	private int port = 0;
 	private String host = "";
-	private String type = "";
 	private Runnable logic = null;
 	private int instanceNumber = 0;
 	private String instanceName = "";
 	private Controller controller = null;
-	private Runnable runnableOpponent = null;
-	private boolean automatedOpponent = false;
-	private Controller automatedController = null;
-	private Logger logger = LogManager.getLogger(GUIDriver.class);
 	
 	private static final String TYPE = "type";
-	private static final String HOST = "host";
-	private static final String PORT = "port";
 	private static final String ERROR = "error";
+	private static final String HOST_ARG = "host";
+	private static final String PORT_ARG = "port";
 	private static final String AUTOMATED = "automatedOpponent";
+	private static final Logger logger = LogManager.getLogger(GUIDriver.class);
 	
 	/**
 	 * This method is called immediately after the Application class is loaded and constructed and prior to the 
@@ -45,13 +42,15 @@ public class GUIDriver extends Application
 	 */
 	@Override
 	public void init()
-	{	
+	{
+		String type = "";
+		boolean automatedOpponent = false;
 		Parameters params = getParameters();
 		Map<String, String> kwargList = params.getNamed();
-		
-		host = kwargList.get(HOST);
+
 		type = kwargList.get(TYPE);
-		port = Integer.parseInt(kwargList.get(PORT));
+		host = kwargList.get(HOST_ARG);
+		port = Integer.parseInt(kwargList.get(PORT_ARG));
 		automatedOpponent = Boolean.parseBoolean(kwargList.get(AUTOMATED));
 		
 		logger.info("Host: {}.", host);
@@ -81,8 +80,8 @@ public class GUIDriver extends Application
 		controller = new BoardController(instanceNumber);
 		logic = new Player(controller, instanceNumber, host, port);
 		instanceName = ((Player) logic).getName();
-		
-		logger.debug("Controller ID: " + controller.getID());
+
+		logger.debug("Controller ID: {}", controller.getID());
 		
 		// If the automated opponent flag is set, an inverse automated player will also be instantiated.
 		if (automatedOpponent)
@@ -110,74 +109,81 @@ public class GUIDriver extends Application
 	 * load the Battleship layout FXML, and set the scene.
 	 */
 	@Override
-	public void start(Stage primaryStage) throws Exception
+	public void start(Stage primaryStage)
 	{
 		Parent root = null;
-		Scene scene = null;
-		FXMLLoader loader = null;
-		
+
+		// Instantiate and load controller
+		FXMLLoader loader = new FXMLLoader();
+		loader.setController(controller);
+
+		// Set the stage title and set the location of the layout to be loaded
+		primaryStage.setTitle("Network Battleship " + instanceName);
+		loader.setLocation(getClass().getResource("layout/BattleshipLayout.fxml"));
+
 		try
 		{
-			// Instantiate and load controller
-			loader = new FXMLLoader();
-			loader.setController(controller);
-			
-			// Set the stage title and set the location of the layout to be loaded
-			primaryStage.setTitle("Network Battleship " + instanceName);
-			loader.setLocation(getClass().getResource("layout/BattleshipLayout.fxml"));
-			
 			// Load the layout configured
 			root = loader.load();
-			
-			// Create a scene based on the loaded layout, grab css resources and set the scene for the primary stage.
-			scene = new Scene(root, 800, 600);
-			scene.getStylesheets().add(getClass().getResource("styles/application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			
-			// Depending on who's turn it is, create a pop up signifying whos turn it is first.
-			if(controller.getCurrentTurn() == instanceNumber)
-			{
-				((Player) logic).infoBox("It is your turn first!", "Player " + Integer.toString(instanceNumber));
-			}
-			else
-			{
-				((Player) logic).infoBox("It is the opponents turn first...", "Player " + Integer.toString(instanceNumber));
-			}
-			
-			// Disables maximizing the primary stage
-			primaryStage.maximizedProperty().addListener((observable, oldValue, newValue) ->
-			{
-	            if (newValue)
-	            {
-	            	primaryStage.setMaximized(false);
-	            }
-	        });
-			
-			// On close of stage, trigger the controller's shutdown function.
-			primaryStage.setOnCloseRequest(event -> controller.shutdown());
-			
-			// Show the scene
-			primaryStage.show();
 		}
 		catch(Exception e)
 		{
 			logger.error(e, e);
 		}
+
+		if (root != null)
+		{
+			// Create a scene based on the loaded layout, grab css resources and set the scene for the primary stage.
+			Scene scene = new Scene(root, 800, 600);
+
+			scene.getStylesheets().add(Objects.requireNonNull(
+					getClass().getResource("styles/application.css")).toExternalForm());
+			primaryStage.setScene(scene);
+
+			// Depending on whose turn it is, create a pop-up signifying whose turn it is first.
+			if (controller.getCurrentTurn() == instanceNumber)
+			{
+				((Player) logic).infoBox("It is your turn first!", "Player " + instanceNumber);
+			}
+			else
+			{
+				((Player) logic).infoBox("It is the opponents turn first...", "Player " + instanceNumber);
+			}
+
+			// Disables maximizing the primary stage
+			primaryStage.maximizedProperty().addListener((observable, oldValue, newValue) ->
+			{
+				if (newValue)
+				{
+					primaryStage.setMaximized(false);
+				}
+			});
+
+			// On close of stage, trigger the controller's shutdown function.
+			primaryStage.setOnCloseRequest(event -> controller.shutdown());
+
+			// Show the scene
+			primaryStage.show();
+		}
 	}
 	
 	/**
-	 * 
-	 * @param playerID
+	 * Initializes an automated opponents' appropriate threads in order to play against the user.
+	 *
+	 * @param playerID - A number representation of whether a client or server instance is initializing.
+	 * @param automatedControllerName - A String representing the players' name.
+	 * @param automatedThreadName - A String representing the threads' name.
 	 */
 	private void startAutomatedOpponent(int playerID, String automatedControllerName, String automatedThreadName)
 	{
 		// Instantiate automated controller and runnable player instance of either server or client playerID
-		automatedController = new AutomatedController(playerID);
-		runnableOpponent = new Player(automatedController, playerID, host, port);
-		((Player) runnableOpponent).setAutomated(true);
+		Controller automatedController = new AutomatedController(playerID);
+		Player runnableOpponent = new Player(automatedController, playerID, host, port);
+		runnableOpponent.setAutomated(true);
 		
-		logger.debug("Automated Controller ID: " + automatedController.getID());
-		
+		logger.debug("Automated Controller ID: {}", automatedController.getID());
+
+		// Instantiate and start automated controller thread
 		Thread automatedLogic = new Thread((Runnable) automatedController);
 		automatedLogic.setName(automatedControllerName);
 		automatedLogic.start();
@@ -189,7 +195,10 @@ public class GUIDriver extends Application
 	}
 	
 	/**
-	 * 
+	 * The main initialization logic for creating and starting a new logic player thread for either a client
+	 * or a server.
+	 *
+	 * @param threadName - A String representing the type of player (server or client) that has started.
 	 */
 	private void startLogic(String threadName)
 	{
