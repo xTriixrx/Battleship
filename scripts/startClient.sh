21fea5deb2a3ed804538425bf7783c5450fd2d5e
xTriixrx <vnigro5743@gmail.com>
2022-10-26 23:01:27 -0400

    Finally fixed personal issue with start bash scripts, basically was a kernel issue

--- scripts/startClient.sh
+++ scripts/startClient.sh
@@ -5,15 +5,17 @@ PORT="$4"
 OPPONENT="$5"
 GAME_LIB="$2"
 JAVAFX_LIB="$1"
+LOG4J_FILE_PATH="log4j2.xml"
 
 if [ $# -ne 5 ]; then
 	echo "Expected 5 parameters but only received $# parameters."
+	echo "For logging support, it is expected the log4j2.xml file should exist in current working directory."
 	echo "./startClient.sh [JAVAFX_LIB] [GAME_LIB] [HOST] [PORT] [AUTOMATED]"
 	echo "./startClient.sh javafx-17.0.2/lib lib 192.168.0.1 5000 false"
 	exit 1
 fi
 
-java -Dlog4j2.disable.jmx="TRUE" --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
+java -Dlog4j.configurationFile=$LOG4J_FILE_PATH --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
 -cp "$JAVAFX_LIB/*:$GAME_LIB/*" com.qfi.battleship.GUIDriverRunner --type=client --port=$PORT --host=$HOST --automatedOpponent=$OPPONENT &
 
 exit 0
--- scripts/startServer.sh
+++ scripts/startServer.sh
@@ -4,15 +4,17 @@ PORT="$3"
 OPPONENT="$4"
 GAME_LIB="$2"
 JAVAFX_LIB="$1"
+LOG4J_FILE_PATH="log4j2.xml"
 
 if [ $# -ne 4 ]; then
 	echo "Expected 4 parameters but only received $# parameters."
+	echo "For logging support, it is expected the log4j2.xml file should exist in current working directory."
 	echo "./startServer.sh [JAVAFX_LIB] [GAME_LIB] [PORT] [AUTOMATED]"
 	echo "./startServer.sh javafx-17.0.2/lib lib 5000 false"
 	exit 1
 fi
 
-java -Dlog4j2.disable.jmx="TRUE" --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
+java -Dlog4j.configurationFile=$LOG4J_FILE_PATH --module-path $JAVAFX_LIB --add-modules javafx.controls,javafx.fxml \
 -cp "$JAVAFX_LIB/*:$GAME_LIB/*" com.qfi.battleship.GUIDriverRunner --type=server --port=$PORT --host=127.0.0.1 --automatedOpponent=$OPPONENT &
 
 exit 0
