af731226245c1e4df2b5f34a83a76ee40286d762
xTriixrx <vnigro5743@gmail.com>
2022-11-01 19:58:24 -0400

    Player class refactoring & cleanup

--- src/com/qfi/battleship/Player.java
+++ src/com/qfi/battleship/Player.java
@@ -18,23 +18,20 @@ import org.apache.log4j.LogManager;
  */
 public class Player implements Runnable, Observable, Observer
 {
-	private int port = 0;
-	private int myID = 0;
-	private int lineCount = 0;
-	private String myName = "";
-	private String address = "";
-	private int currentTurn = 0;
-	private boolean over = false;
-	private Socket socket = null;
-	private Observer observer = null;
-	private boolean automated = false;
-	private DataInputStream in = null;
-	private SecureRandom random = null;
-	private ServerSocket server = null;
-	private DataOutputStream out = null;
-	private Controller controller = null;
-	private final Object shipSetSignal = new Object();
-	private Logger logger = LogManager.getLogger(Player.class);
+	private int m_port = 0;
+	private int m_myID = 0;
+	private String m_myName = "";
+	private String m_address = "";
+	private boolean m_over = false;
+	private Socket m_socket = null;
+	private Observer m_observer = null;
+	private boolean m_automated = false;
+	private DataInputStream m_in = null;
+	private ServerSocket m_server = null;
+	private DataOutputStream m_out = null;
+	private Controller m_controller = null;
+	private final Object m_shipSetSignal = new Object();
+	private static final Logger m_logger = LogManager.getLogger(Player.class);
 
 	public static final int CLIENT_ID = 1;
 	public static final int SERVER_ID = 2;
@@ -43,28 +40,31 @@ public class Player implements Runnable, Observable, Observer
 	public static final String CLIENT = "client";
 
 	/**
-	 * 
-	 * @param playerType
-	 * @param address
-	 * @param port
+	 * Player constructor.
+	 *
+	 * @param controller - Some controller interface in order to pass/receive game updates.
+	 * @param playerType - The type of player this player instance will be.
+	 * @param address - The address of the expected server instance.
+	 * @param port - The port of the expected server instance.
 	 */
 	public Player(Controller controller, int playerType, String address, int port)
 	{
 		if (playerType == 1) // client
 		{
-			myID = CLIENT_ID;
-			myName = capitalize(CLIENT);
+			m_myID = CLIENT_ID;
+			m_myName = capitalize(CLIENT);
 		}
 		else if (playerType == 2) // server
 		{
-			myID = SERVER_ID;
-			myName = capitalize(SERVER);
+			m_myID = SERVER_ID;
+			m_myName = capitalize(SERVER);
 		}
 
-		this.port = port;
-		this.address = address;
-		this.controller = controller;
-		
+		m_port = port;
+		m_address = address;
+		m_controller = controller;
+
+		// Establish bidirectional observer message pattern
 		((Observable) controller).register(this);
 		register((Observer) controller);
 
@@ -73,12 +73,11 @@ public class Player implements Runnable, Observable, Observer
 			.putLong(System.currentTimeMillis()).array();
 		
 		// Instantiate random object
-		random = new SecureRandom(seed);
+		SecureRandom random = new SecureRandom(seed);
 		
-		if (myID == SERVER_ID)
+		if (m_myID == SERVER_ID)
 		{
-			currentTurn = random.nextInt(MAX_PLAYERS) + 1;
-			controller.setCurrentTurn(currentTurn);
+			controller.setCurrentTurn(random.nextInt(MAX_PLAYERS) + 1);
 		}
 	}
 
@@ -88,60 +87,67 @@ public class Player implements Runnable, Observable, Observer
 	@Override
 	public void run()
 	{
+		int lineCount = 0;
 		initializeConnection();
 		initializeStreams();
 
-		observer.update("CONNECTED");
+		m_observer.update("CONNECTED");
 
 		// string to read message from input 
 		String message = "";
 
-		while (!over)
+		while (!m_over)
 		{
 			try
 			{
 				// If no lines have been read, the initial handshake needs to take place
 				if (lineCount == 0)
 				{
+					//
 					performHandshake();
-					
+
+					//
+					isShipsSet();
+
 					try
 					{
-						logger.debug("Player " + myID + " is sending READY to opponent.");
-						out.writeUTF("READY");
-						out.flush();
+						m_logger.debug("Player " + m_myID + " is sending READY to opponent.");
+						m_out.writeUTF("READY");
+						m_out.flush();
 					}
 					catch (Exception e)
 					{
 
 					}
 
-					while (!(in.readUTF()).equals("READY"))
+					while (!(m_in.readUTF()).equals("READY"))
 					{
 						try
 						{
-							logger.debug("Player " + myID + " waiting for incoming READY from opponent.");
+							m_logger.debug("Player " + m_myID + " waiting for incoming READY from opponent.");
 							Thread.sleep(500);
 						}
 						catch (Exception e)
 						{
-							logger.error(e, e);
+							m_logger.error(e, e);
 						}
 					}
 
-					observer.update("SET");
+					m_observer.update("SET");
 				}
 				else // Process message by opponent
 				{
-					message = in.readUTF();
-					logger.debug("Player received '" + message + "' message from opponent.");
+					message = m_in.readUTF();
+					m_logger.debug("Player received '" + message + "' message from opponent.");
 					received(message);
-					observer.update(message);
+					m_observer.update(message);
 				}
+
+				lineCount++;
 			}
 			catch (Exception e)
 			{
-				logger.error(e, e);
+				m_logger.error(e, e);
 			}
 		}
 
@@ -153,11 +159,11 @@ public class Player implements Runnable, Observable, Observer
 	 */
 	private void performHandshake()
 	{
-		if (myID == SERVER_ID) // server
+		if (m_myID == SERVER_ID) // server
 		{
 			performServerHandshakeProtocol();
 		}
-		else if (myID == CLIENT_ID) // client
+		else if (m_myID == CLIENT_ID) // client
 		{
 			performClientHandshakeProtocol();
 		}
@@ -172,16 +178,14 @@ public class Player implements Runnable, Observable, Observer
 		
 		try
 		{
-			out.writeInt(currentTurn);
-			out.flush();
-			line = in.readUTF(); 
-			logger.info("Server received: " + line + " from client.");
-			lineCount++;
-			isShipsSet();
+			m_out.writeInt(m_controller.getCurrentTurn());
+			m_out.flush();
+			line = m_in.readUTF();
+			m_logger.info("Server received: " + line + " from client.");
 		}
 		catch (Exception e)
 		{
-			logger.error(e, e);
+			m_logger.error(e, e);
 		}
 	}
 	
@@ -192,67 +196,14 @@ public class Player implements Runnable, Observable, Observer
 	{
 		try
 		{
-			currentTurn = in.readInt();
-			out.writeUTF("Client received " + currentTurn + " from Server...");
-			out.flush();
-			controller.setCurrentTurn(currentTurn);
-			lineCount++;
-			isShipsSet();
+			int currentTurn = m_in.readInt();
+			m_out.writeUTF("Client received " + currentTurn + " from Server...");
+			m_out.flush();
+			m_controller.setCurrentTurn(currentTurn);
 		}
 		catch (Exception e)
 		{
-			logger.error(e, e);
-		}
-	}
-	
-	/**
-	 * 
-	 */
-	private void initializeConnection()
-	{
-		if (myID == SERVER_ID)
-		{
-			initializeServerSocket();
-		}
-		else if (myID == CLIENT_ID)
-		{
-			connectToServer();
-		}
-	}
-	
-	/**
-	 * 
-	 */
-	private void initializeStreams()
-	{
-		try
-		{
-			// takes input from the client socket 
-			in = new DataInputStream(socket.getInputStream());
-			out = new DataOutputStream(socket.getOutputStream());
-		}
-		catch (Exception e)
-		{
-			logger.error(e, e);
-		}
-	}
-	
-	/**
-	 * 
-	 */
-	private void initializeServerSocket()
-	{
-		try
-		{
-			server = new ServerSocket(port);
-			logger.info("Local Address: " + serverAddress());
-			logger.info("Waiting for a client to connect.");
-			socket = server.accept(); 
-			logger.info("Client accepted."); 
-		}
-		catch (Exception e)
-		{
-			logger.error(e, e);
+			m_logger.error(e, e);
 		}
 	}
 	
@@ -263,21 +214,20 @@ public class Player implements Runnable, Observable, Observer
 	{
 		try
 		{
-			socket = new Socket(address, port); 
-			logger.info("Connected"); 
+			m_socket = new Socket(m_address, m_port);
+			m_logger.info("Connected to server at " + m_address + ":" + m_port + "!");
 		}
 		catch (Exception e)
 		{
-			logger.error(e, e);
+			m_logger.error(e, e);
 		}
 	}
 
 	@Override
-	public void update(String s)
+	public void update(String controllerMessage)
 	{
-		logger.info("Received " + s + " from observable controller.");
-		StringBuilder t = new StringBuilder(s);
-		updateResponse(s, t);
+		m_logger.info("Received " + controllerMessage + " from observable controller.");
+		updateResponse(controllerMessage);
 	}
 
 	/**
@@ -292,7 +242,7 @@ public class Player implements Runnable, Observable, Observer
 		}
 		else if(opponentMessage.equals("OVER"))
 		{
-			infoBox("You Won! (:", "Player " + myID);
+			infoBox("You Won! (:", "Player " + m_myID);
 			
 			try
 			{
@@ -301,46 +251,45 @@ public class Player implements Runnable, Observable, Observer
 			}
 			catch (Exception e)
 			{
-				logger.error(e, e);
+				m_logger.error(e, e);
 			}
-			
-			over = true;
+
+			m_over = true;
 		}
 		else if(opponentMessage.equals(Armada.CARRIER_NAME))
 		{
-			infoBox("You sunk your opponents Carrier!", "Player " + myID);
+			infoBox("You sunk your opponents Carrier!", "Player " + m_myID);
 		}
 		else if(opponentMessage.equals(Armada.BATTLESHIP_NAME))
 		{
-			infoBox("You sunk your opponents Battleship!", "Player " + myID);
+			infoBox("You sunk your opponents Battleship!", "Player " + m_myID);
 		}
 		else if(opponentMessage.equals(Armada.CRUISER_NAME))
 		{
-			infoBox("You sunk your opponents Cruiser!", "Player " + myID);
+			infoBox("You sunk your opponents Cruiser!", "Player " + m_myID);
 		}
 		else if(opponentMessage.equals(Armada.SUBMARINE_NAME))
 		{
-			infoBox("You sunk your opponents Submarine!", "Player " + myID);
+			infoBox("You sunk your opponents Submarine!", "Player " + m_myID);
 		}
 		else if(opponentMessage.equals(Armada.DESTROYER_NAME))
 		{
-			infoBox("You sunk your opponents Destroyer!", "Player " + myID);
+			infoBox("You sunk your opponents Destroyer!", "Player " + m_myID);
 		}
 	}
 
 	/**
-	 * 
-	 * @param str
-	 * @param build
+	 *
+	 * @param controllerMessage
 	 */
-	public void updateResponse(String str, StringBuilder build)
+	public void updateResponse(String controllerMessage)
 	{
-		if (str.equalsIgnoreCase("SHUTDOWN"))
+		if (controllerMessage.equalsIgnoreCase("SHUTDOWN"))
 		{
 			try
 			{
-				out.writeUTF(str);
-				out.flush();
+				m_out.writeUTF(controllerMessage);
+				m_out.flush();
 			}
 			catch (Exception e)
 			{
@@ -349,69 +298,60 @@ public class Player implements Runnable, Observable, Observer
 
 			System.exit(0);
 		}
-		else if (str.equals("SHIPS"))
+		else if (controllerMessage.equals("SHIPS"))
 		{
-			synchronized (shipSetSignal)
+			synchronized (m_shipSetSignal)
 			{
-				shipSetSignal.notifyAll();
+				m_shipSetSignal.notifyAll();
 			}
 		}
-		else if (str.equals("OVER"))
+		else if (controllerMessage.equals("OVER"))
 		{
-			infoBox("You lost :(", "Player " + myID);
-			over = true;
+			infoBox("You lost :(", "Player " + m_myID);
+			m_over = true;
 		}
-		else if (str.contains(Armada.CARRIER_NAME))
+		else if (controllerMessage.contains(Armada.CARRIER_NAME))
 		{
-			infoBox("Your Carrier has been sunk!", "Player " + myID);
+			infoBox("Your Carrier has been sunk!", "Player " + m_myID);
 		}
-		else if (str.contains(Armada.BATTLESHIP_NAME))
+		else if (controllerMessage.contains(Armada.BATTLESHIP_NAME))
 		{
-			infoBox("Your Battleship has been sunk!", "Player " + myID);
+			infoBox("Your Battleship has been sunk!", "Player " + m_myID);
 		}
-		else if (str.contains(Armada.CRUISER_NAME))
+		else if (controllerMessage.contains(Armada.CRUISER_NAME))
 		{
-			infoBox("Your Cruiser has been sunk!", "Player " + myID);
+			infoBox("Your Cruiser has been sunk!", "Player " + m_myID);
 		}
-		else if (str.contains(Armada.SUBMARINE_NAME))
+		else if (controllerMessage.contains(Armada.SUBMARINE_NAME))
 		{
-			infoBox("Your Submarine has been sunk!", "Player " + myID);
+			infoBox("Your Submarine has been sunk!", "Player " + m_myID);
 		}
-		else if (str.contains(Armada.DESTROYER_NAME))
+		else if (controllerMessage.contains(Armada.DESTROYER_NAME))
 		{
-			infoBox("Your Destroyer has been sunk!", "Player " + myID);
+			infoBox("Your Destroyer has been sunk!", "Player " + m_myID);
 		}
-		else if (build.length() == 4)
+		else
 		{
-			if (build.charAt(3) == myID)
+			if (controllerMessage.charAt(controllerMessage.length() - 1) == m_myID)
 			{
-				build.deleteCharAt(3);
-				str = build.toString();
-				currentTurn = myID;
-			}
-		}
-		else if (build.length() == 5)
-		{
-			if (build.charAt(4) == myID)
-			{
-				build.deleteCharAt(4);
-				str = build.toString();
-				currentTurn = myID;
+				StringBuilder sb = new StringBuilder(controllerMessage);
+				sb.deleteCharAt(sb.length() - 1);
+				controllerMessage = sb.toString();
 			}
 		}
 
 		try
 		{
-			if (!str.equals("SHIPS"))
+			if (!controllerMessage.equals("SHIPS"))
 			{
-				logger.info("Sending '" + str + "' to opponent.");
-				out.writeUTF(str);
-				out.flush();
+				m_logger.info("Sending '" + controllerMessage + "' to opponent.");
+				m_out.writeUTF(controllerMessage);
+				m_out.flush();
 			}
 		}
 		catch (Exception e)
 		{
-			logger.error(e, e);
+			m_logger.error(e, e);
 		}
 	}
 
@@ -424,23 +364,23 @@ public class Player implements Runnable, Observable, Observer
 
 		while (!isSet)
 		{
-			synchronized (shipSetSignal)
+			synchronized (m_shipSetSignal)
 			{
 				try
 				{
-					shipSetSignal.wait();
+					m_shipSetSignal.wait();
 				}
 				catch (InterruptedException e)
 				{
-					logger.error(e, e);
+					m_logger.error(e, e);
 					Thread.currentThread().interrupt();
 				}
 			}
 
-			logger.info("All ships are set!");
+			m_logger.info("All ships are set!");
 			isSet = true;
-			controller.getArmada().logArmadaPosition();
-			infoBox("Ships are Set!", "Player " + myID);
+			m_controller.getArmada().logArmadaPosition();
+			infoBox("Ships are Set!", "Player " + m_myID);
 		}
 	}
 
@@ -459,7 +399,7 @@ public class Player implements Runnable, Observable, Observer
 		}
 		catch (Exception e)
 		{
-			logger.error(e, e);
+			m_logger.error(e, e);
 		}
 
 		if (temp != null)
@@ -479,65 +419,97 @@ public class Player implements Runnable, Observable, Observer
 		// close the connections
 		try
 		{
-			controller.shutdown();
+			m_controller.shutdown();
+
+			m_in.close();
+			m_out.close();
+			m_socket.close();
 			
-			in.close(); 
-			out.close(); 
-			socket.close();
-			
-			if (server != null)
+			if (m_server != null)
 			{
-				server.close();
+				m_server.close();
 			}
 		}
 		catch (Exception e)
-		{ 
-			logger.error(e, e); 
+		{
+			m_logger.error(e, e);
 		}
-	}
-	
-	/**
-	 * 
-	 * @return
-	 */
-	public boolean isAutomated()
-	{
-		return automated;
-	}
-	
-	/**
-	 * 
-	 * @param automated
-	 */
-	public void setAutomated(boolean automated)
-	{
-		this.automated = automated;
 	}
 
 	/**
-	 * 
-	 * @param observer
+	 *
 	 */
-	@Override
-	public void register(Observer observer)
+	private void initializeConnection()
 	{
-		this.observer = observer;
+		if (m_myID == SERVER_ID)
+		{
+			initializeServerSocket();
+		}
+		else if (m_myID == CLIENT_ID)
+		{
+			connectToServer();
+		}
 	}
-	
+
 	/**
-	 * 
-	 * @return String
+	 * Instantiates and initializes the server socket for a client to connect to, will block
+	 * until the client connection has been established.
 	 */
-	public String getName()
+	private void initializeServerSocket()
 	{
-		return myName;
+		try
+		{
+			m_server = new ServerSocket(m_port);
+			m_logger.info("Local Address: " + serverAddress());
+			m_logger.info("Waiting for a client to connect...");
+
+			// Blocks waiting for a client to connect to the socket
+			m_socket = m_server.accept();
+			m_logger.info("Client accepted.");
+		}
+		catch (Exception e)
+		{
+			m_logger.error(e, e);
+		}
 	}
-	
+
+	/**
+	 * Instantiates and initializes the data input and output streams to communicate through
+	 * the socket interface.
+	 */
+	private void initializeStreams()
+	{
+		try
+		{
+			// takes input from the client socket
+			m_in = new DataInputStream(m_socket.getInputStream());
+			m_out = new DataOutputStream(m_socket.getOutputStream());
+		}
+		catch (Exception e)
+		{
+			m_logger.error(e, e);
+		}
+	}
+
+	/**
+	 * A Java Swing pop up info box for showing basic pop up information to the user during the game.
+	 * 
+	 * @param infoMessage - The message to be inserted into the pop-up message.
+	 * @param titleBar - The partial title of the pop-up box.
+	 */
+	public void infoBox(String infoMessage, String titleBar)
+	{
+		if (!isAutomated())
+		{
+			JOptionPane.showMessageDialog(null, infoMessage, m_myName + ": " + titleBar, JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+
 	/**
 	 * Capitalizes a provided string by making the first character in the string uppercase.
-	 * 
+	 *
 	 * @param string The string to capitalize.
-	 * @return String
+	 * @return String - A capitalized string.
 	 */
 	public String capitalize(String string)
 	{
@@ -545,16 +517,43 @@ public class Player implements Runnable, Observable, Observer
 	}
 
 	/**
-	 * An Java Swing pop up info box for showing basic pop up information to the user during the game.
-	 * 
-	 * @param infoMessage The message to be inserted into the pop up message.
-	 * @param titleBar The partial title of the pop up box.
+	 * Registers an observer object instance to this observable instance.
+	 *
+	 * @param observer - An observer interface to exchange messages between the observer & the observable.
 	 */
-	public void infoBox(String infoMessage, String titleBar)
+	@Override
+	public void register(Observer observer)
 	{
-		if (!isAutomated())
-		{
-			JOptionPane.showMessageDialog(null, infoMessage, myName + ": " + titleBar, JOptionPane.INFORMATION_MESSAGE);
-		}
+		m_observer = observer;
+	}
+
+	/**
+	 * A mutator for setting whether the player is communicating with an automated controller.
+	 *
+	 * @param automated - A flag representing if the controller is automated.
+	 */
+	public void setAutomated(boolean automated)
+	{
+		m_automated = automated;
+	}
+
+	/**
+	 * An accessor returning a string based name representing the type of player instance.
+	 *
+	 * @return String - A string representing the name of the type of player that this instance represents.
+	 */
+	public String getName()
+	{
+		return m_myName;
+	}
+
+	/**
+	 * A boolish method representing whether an automated controller is communicating with this player instance.
+	 *
+	 * @return boolean - A flag representing whether this player is representing an automated controller.
+	 */
+	public boolean isAutomated()
+	{
+		return m_automated;
 	}
 }
